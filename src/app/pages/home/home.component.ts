c66853a6d0f5f397ff98806f933a7dfa6264f37f
Golden <goldenprieto@gmail.com>
2021-03-10 22:54:43 -0500

    aplicacion finalizada

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from '../../services/youtube.service';
+import { Video } from '../../models/youtube.models';
+
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
 
 
 
@@ -10,13 +14,47 @@ import { YoutubeService } from '../../services/youtube.service';
 })
 export class HomeComponent implements OnInit {
 
+ 
+  
+  videos:Video[]=[]
+
   constructor( private youTubeServices : YoutubeService) { }
 
   ngOnInit() {
+    this.cargarVideos();
+  }
+
+  cargarVideos(){
     this.youTubeServices.getVideos().subscribe(resp =>{
-      console.log(resp.items[1].snippet.title);
+      
+      this.videos.push(...resp);
+      console.log(this.videos);
+      
       
     })
   }
 
+  mostrarVideo(video: Video){
+    console.log(video);
+    Swal.fire({
+    
+      html:
+        `
+        <div clas="text-center"><strong>Titulo :</strong> <u>${ video.title }</u></div>
+        <hr>
+        <iframe 
+            width="100%" 
+            height="315" 
+            src="https://www.youtube.com/embed/${video.resourceId.videoId}" 
+            frameborder="0" allow="accelerometer; 
+            autoplay; 
+            clipboard-write;
+            encrypted-media;
+            gyroscope; 
+            picture-in-picture" allowfullscreen>
+          </iframe>`
+    })
+    
+  }
+  
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient,HttpParams } from '@angular/common/http'
-import { YoutubeResponde } from '../models/youtube.models';
+import { YoutubeResponde, Video } from '../models/youtube.models';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class YoutubeService {
   private youTubeUrl ='https://www.googleapis.com/youtube/v3' ;
   private apiKey ="AIzaSyDmOaN1QoxE-m4MQNIw31vUzQcEX7m-FQ4";
   private playlist= "UUuaPTYj15JSkETGnEseaFFg";
-  private nextPageToken= "";
+  private nextPageToken= '';
 
 
   constructor(
@@ -25,7 +26,20 @@ export class YoutubeService {
               .set('key',this.apiKey)
               .set('playlistId',this.playlist)
               .set('maxResults','10')
+              .set('pageToken',this.nextPageToken)
+
 
     return this.http.get<YoutubeResponde>(url,{ params : params })
+                      .pipe(map( resp => {
+                          this.nextPageToken = resp.nextPageToken;
+                          return resp.items;
+                          
+                      }),
+                      map(items =>{
+                        console.log(this.nextPageToken);
+                        
+                        return items.map(video => video.snippet)
+                      })
+                    )
   }
 }
